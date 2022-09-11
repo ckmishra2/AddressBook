fdb64cb4fc6523b863ed10010ee16dab4323d39e
ckmishra2 <chandrakalatiwari2@gmail.com>
2022-09-11 22:55:33 +0530

    Add: Creating a contact in address book

--- AddressBook.java
+++ AddressBook.java
@@ -1,7 +1,36 @@
 package basics;
+class PersonDetails{
+	String firstName, lastName,city, state,email;
+	int dateOfBirth, phnNumber,zip;
+//creating a Contacts in Address Book with first and last names, address,city, state, zip, phone number and email...
+	PersonDetails(String first, String Last, int dob, int phnnum,String yourcity, String yourState,int pincode,String emal){
+		this.firstName=first;
+		this.lastName=Last;
+		this.dateOfBirth=dob;
+		this.phnNumber=phnnum;
+		this.city=yourcity;
+		this.state=yourState;
+		this.zip=pincode;
+		this.email=emal;
+	}
+
+	void printDetails() {
+		System.out.println("First name is: "+this.firstName);
+		System.out.println("Last name is: "+this.lastName);
+		System.out.println("Date of birth is: "+this.dateOfBirth);
+		System.out.println("Phone number is: "+this.phnNumber);
+		System.out.println("City name is: "+ this.city);
+		System.out.println("State name is: "+this.state);
+		System.out.println("Zip is: "+this.zip);
+		System.out.println("email is "+this.email);
+
+	}
+}
 public class AddressBook{
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book Program");
+		PersonDetails P1 = new  PersonDetails( "calli", "Tripathi", 10021996,  27823, "pune",  "Maharastra", 200112,"kt@gmail.com");
+		P1.printDetails();
 	}
-	
 }
+
